98beeff5c283085b44d147acd9234193b6237870
vtrpaulon <vtrpaulon@gmail.com>
2023-08-22 08:58:55 -0300

    conclusao do codigo desafio conta terminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner teclado = new Scanner(System.in);
+        
+        int agencia;
+        int numeroConta;
+        String cliente;
+        double saldo;
+        
+        System.out.println("Informe o numero da agencia: ");
+        agencia = teclado.nextInt();
+        
+        System.out.println("Informe o numero da conta: ");
+        numeroConta = teclado.nextInt();
+        
+        System.out.println("Informe o nome do cliente: ");
+        cliente = (String)teclado.next();
+        
+        System.out.println("Qual é o valor do deposito inicial: ");
+        saldo = teclado.nextDouble();
+        
+        System.out.println("Prezado cliente "+cliente+", agencia "+agencia+", conta "+numeroConta+", seu saldo atual é "+saldo);
     }
 }
